3db5f1b42ddeefa9206225e04585fc2fee90ab03
Priya <rspdd2004@gmail.com>
2026-06-28 21:01:15 +0530

    Modification

--- billing.js
+++ billing.js
@@ -12,6 +12,155 @@ const dateTime =
 document.getElementById("dateTime").value =
     dateTime;
 
+}
+function printBill() {
+
+    const amount = document.getElementById("amount").value;
+    const paymentMode = document.querySelector('input[name="paymentMode"]:checked').value;
+    const description = document.getElementById("description").value;
+
+    const now = new Date();
+
+    const date = now.toLocaleDateString("en-GB");
+    const time = now.toLocaleTimeString();
+
+    const bill = `
+    <html>
+    <head>
+    <title>SSCSC eSevai Center Bill</title>
+
+    <style>
+
+    body{
+        font-family:Calibri;
+        margin:40px;
+        color:#000;
+    }
+
+    h2,h3,p{
+        text-align:center;
+        margin:4px;
+    }
+
+    table{
+        width:100%;
+        border-collapse:collapse;
+        margin-top:25px;
+    }
+
+    table,th,td{
+        border:1px solid black;
+    }
+
+    th,td{
+        padding:10px;
+        text-align:center;
+    }
+
+    .footer{
+
+        margin-top:70px;
+
+        display:flex;
+
+        justify-content:space-between;
+
+    }
+
+    </style>
+
+    </head>
+
+    <body>
+
+        <h2>SSCSC eSevai Center</h2>
+
+        <p><b>Village :</b> Thadicombu</p>
+
+        <p><b>City :</b> Dindigul</p>
+
+        <p><b>Owner :</b> Selvanayaki</p>
+
+        <p><b>WhatsApp :</b> 8695957345</p>
+
+        <hr>
+
+        <table>
+
+        <tr>
+
+        <th>Date</th>
+
+        <th>Time</th>
+
+        <th>Amount</th>
+
+        <th>Payment Mode</th>
+
+        <th>Description</th>
+
+        </tr>
+
+        <tr>
+
+        <td>${date}</td>
+
+        <td>${time}</td>
+
+        <td>₹ ${amount}</td>
+
+        <td>${paymentMode}</td>
+
+        <td>${description}</td>
+
+        </tr>
+
+        </table>
+
+        <br>
+
+        <h3>Total Amount : ₹ ${amount}</h3>
+
+        <div class="footer">
+
+            <div>
+
+                ______________________
+
+                <br><br>
+
+                Shop Seal
+
+            </div>
+
+            <div>
+
+                ______________________
+
+                <br><br>
+
+                Owner Signature
+
+            </div>
+
+        </div>
+
+    </body>
+
+    </html>
+    `;
+
+    var win = window.open("", "", "width=900,height=700");
+
+    win.document.write(bill);
+
+    win.document.close();
+
+    win.focus();
+
+    win.print();
+
+    win.close();
 
 }
 
